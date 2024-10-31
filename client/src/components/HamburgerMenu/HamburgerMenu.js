c740a3417ed5d58dac9dec96b0f537df8c97d990
HaydenJones1125 <haydenj1125@yahoo.com>
2024-10-31 15:31:42 -0500

    Changed hamburger menu so it closes the menu when an item is clicked

--- client/src/components/HamburgerMenu/HamburgerMenu.js
+++ client/src/components/HamburgerMenu/HamburgerMenu.js
@@ -7,42 +7,44 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { useAuth } from '../../hooks/AuthProvider';
 import Register from './Register.js';
 
-
 export default props => {
   const [registration, setRegistration] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // State to manage menu open/close
   const auth = useAuth();
 
+  // Function to close menu
+  const closeMenu = () => setIsOpen(false);
 
   return (
-    <Menu>
+    <Menu isOpen={isOpen} onStateChange={({ isOpen }) => setIsOpen(isOpen)}>
       <img src={logoPath} alt="Logo" className="hamburger-logo" />
-      <a className="menu-item" href="/homepage">
+      <a className="menu-item" href="/homepage" onClick={closeMenu}>
         Home
       </a>
-      <Link className="menu-item" to="/practice">
+      <Link className="menu-item" to="/practice" onClick={closeMenu}>
         Create Practice
       </Link>
-      <Link className="menu-item" to="/game">
+      <Link className="menu-item" to="/game" onClick={closeMenu}>
         Create Game
       </Link>
-      <Link className="menu-item" to="/teamstats">
+      <Link className="menu-item" to="/teamstats" onClick={closeMenu}>
         Team Stats
       </Link>
-      <Link className="menu-item" to="/playerstats">
+      <Link className="menu-item" to="/playerstats" onClick={closeMenu}>
         Player Stats
       </Link>
-      <Link className="menu-item" to="/export">
+      <Link className="menu-item" to="/export" onClick={closeMenu}>
         Export
       </Link>
       {sessionStorage.getItem('site') === 'Admin' && (
-        <button className="menu-item-button" onClick={() => setRegistration(true)}>
+        <button className="menu-item-button" onClick={() => { setRegistration(true); closeMenu(); }}>
           Create Registration Key
         </button>
       )}
       {registration && (
         <Register isOpen={registration} onClose={() => setRegistration(false)} />
       )}
-      <Link className="menu-item" onClick={() => auth.logOut()} to="/">
+      <Link className="menu-item" onClick={() => { auth.logOut(); closeMenu(); }} to="/">
         Sign Out
       </Link>
     </Menu>
