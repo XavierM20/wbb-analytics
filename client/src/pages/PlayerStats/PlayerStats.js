9c122b47f6542a81cfe26f12db392b3151a4a9dc
agranados02 <a2002granados@gmail.com>
2024-12-08 20:54:22 -0600

    Update PlayerStats.js

--- client/src/pages/PlayerStats/PlayerStats.js
+++ client/src/pages/PlayerStats/PlayerStats.js
@@ -67,7 +67,6 @@ function PlayerStats() {
   const [selectedPlayer, setSelectedPlayer] = useState(''); //
   const serverUrl = process.env.REACT_APP_SERVER_URL;
 
-  const navigate = useNavigate();
 
 useEffect(() => {
   const fetchInitialData = async () => {
