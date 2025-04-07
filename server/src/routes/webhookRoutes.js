4c74587d958af7e9537b9168175f99e9c6e49715
jjComia <142517906+jjComia@users.noreply.github.com>
2025-04-06 20:42:58 -0500

    changes webhook routes,js for server error

--- server/src/routes/webhookRoutes.js
+++ server/src/routes/webhookRoutes.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors")
 const crypto = require("crypto");
- 
-const app = express();
-app.use(express.json())
-const PORT = 9000;
- 
-app.use(bodyParser.json());
- 
-app.get("/github-webhook/alive", (req,res,next) => {
-   res.status(200).json({message:"alive"})
-})
-app.post("/github-webhook/pushevent", (req, res) => {
-    console.log("Received webhook event:", req.body);
-    res.sendStatus(200);
-    processWebhook(req.body);
+
+const router = express.Router();
+
+router.get("/alive", (req, res) => {
+  res.status(200).json({ message: "alive" });
 });
- 
-app.listen(PORT, () => console.log(`Webhook listener running on port ${PORT}`));
+
+router.post("/pushevent", (req, res) => {
+  console.log("Received webhook event:", req.body);
+  res.sendStatus(200);
+});
 module.exports = router;
+
